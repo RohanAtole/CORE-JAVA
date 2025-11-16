ffba3841e4b552c9c819655fd61307214271dcfd
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-16 23:50:17 +0530

    Add AboutMe class with personal information

---
+++ AboutMe.java
@@ -0,0 +1,9 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String name = "Rohan";
+        int age = 21;
+
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " + age);
+    }
+}
