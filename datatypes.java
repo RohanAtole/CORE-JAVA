efae2f7fb6136824249e921265c389315fb91b76
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-17 20:18:10 +0530

    Add datatypes example in Java

---
+++ datatypes.java
@@ -0,0 +1,34 @@
+package Variables_DataTypes;
+
+public class datatypes{
+
+	public static void main(String args[]) {
+	
+	//print values using datatypes
+		
+	//integer
+	int a=10;
+	System.out.println("number is"+a);
+	
+	//string
+	String c="rohan";
+	System.out.println("string is"+c);
+	
+	//double
+	double d=1.5;
+	System.out.println("double number"+d);
+	
+	//long
+	long e=12000000;
+	System.out.println("long number is"+e);
+	
+	//for character
+	char f='R';
+	System.out.println("character is "+f);
+	
+	//for boolean
+	boolean g=true;
+	System.out.println("boolean is "+g);
+	
+}
+}
