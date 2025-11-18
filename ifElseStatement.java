9a63ad40188d372fc20d44253ae15c4ad2569dbc
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-18 19:27:08 +0530

    Modify numbers and made logic for which number is greater than three numbers
    Updated num1 to 100 and added logic to compare three numbers.

--- ifElseStatement.java
+++ ifElseStatement.java
@@ -4,14 +4,27 @@ public class ifElseStatement{
 	
 	public static void main(String args[]) {
 		
-		int num1 = 10;
+		int num1 = 100;
 		int num2 = 20;
+		int num3 = 15;
 		
 		if(num1 < num2) {
 			System.out.println(num2 + "is greater");
 		}
 		else {
 			System.out.println(num1 + "is greater");
+		}
+		
+		//for checking three numbers 
+		
+		if(num1 < num2 && num2 < num3 && num2 < num3) {
+			System.out.println(num3 +"is greater");
+		}
+		else if(num1 < num2 && num2 > num3 && num1 < num3) {
+			System.out.println(num2 +"is greater");
+		}
+		else {
+			System.out.println(num1 +"is greater");
 		}
 		
 	}
