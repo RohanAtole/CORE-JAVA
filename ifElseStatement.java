5e08edfa3b129f20831d4f624fa9ccd80ad83754
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-18 18:34:25 +0530

    implementation of if-else statement to compare two numbers

---
+++ ifElseStatement.java
@@ -0,0 +1,18 @@
+package Variables_DataTypes;
+
+public class ifElseStatement{
+	
+	public static void main(String args[]) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		if(num1 < num2) {
+			System.out.println(num2 + "is greater");
+		}
+		else {
+			System.out.println(num1 + "is greater");
+		}
+		
+	}
+}
