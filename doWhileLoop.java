9b2a5e6227dac490e04809292212b0ae9f0a2571
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-21 00:51:39 +0530

    Add doWhileLoop.java with loop example
    Implement a do-while loop that prints 'hello' and increments i.

---
+++ doWhileLoop.java
@@ -0,0 +1,16 @@
+package Variables_DataTypes;
+
+public class doWhileLoop{
+	
+	public static void main(String args[]) {
+		
+		int i=4;
+		//main difference is i've get i=5 or more then its take onetime output
+		do{
+			System.out.println("hello");
+			i++;
+		}while(i <= 5);
+		
+		System.out.println(i);
+	}
+}
