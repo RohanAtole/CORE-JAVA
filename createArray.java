7f4868f2e7ff403465e41ba537c32d803a132609
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-23 23:32:29 +0530

    Refactor array creation and add printing logic
    Modified the array initialization and added dynamic array creation with a for loop to print elements.

--- createArray.java
+++ createArray.java
@@ -5,10 +5,20 @@ public class createArray{
 	public static void main(String args[]) {
 		
 		//create the array 
-		int num[] = {5,6,7};
+		int num[] = {5,6,7};     //create the array
+		num[2] = 8;              //replace the value
+		System.out.println(num[2]);
 		
-		System.out.println(num[1]);
-		int num1[] = new int[4];
+		//create the array 
+		int nums[] = new int[4];     //dynamic array
+		nums[0] = 4;
+		nums[1] = 2;
+		nums[2] = 5;
+		System.out.println(nums[2]);
 		
+		//print array elements using for loop
+		for(int i = 0; i<=2; i++) {
+			System.out.println(nums[i]);
+		}
 	}
 }
