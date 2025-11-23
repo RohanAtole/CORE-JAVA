a5bcd4534b1772ee8dcaa932e5079f6fe3438b8d
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-23 23:12:11 +0530

    Create createArray.java with array examples
    create and access elements

---
+++ createArray.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class createArray{
+	
+	public static void main(String args[]) {
+		
+		//create the array 
+		int num[] = {5,6,7};
+		
+		System.out.println(num[1]);
+		int num1[] = new int[4];
+		
+	}
+}
