581beba47d307d807a466fde86b05e8fb2235d9d
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-24 19:21:29 +0530

    Populate and access elements in multidimensional array
    Added functionality to populate and access a multidimensional array with random integers.

--- multidimentionalArray.java
+++ multidimentionalArray.java
@@ -4,15 +4,27 @@ public class multidimentionalArray{
 	public static void main(String args[]) {
 		
 		int nums[][] = new int [3][4];
-		//nums[0][0] = [5];
 		
-		//System.out.println(nums[0]);
-		
+		//get random elements
+		System.out.println("pick random variables\n");
 		for(int i=0; i<3; i++) {
 			for(int j=0; j<4; j++) {
-				System.out.print(nums[i][j] + "");
+				nums[i][j] =  (int)(Math.random() * 10);
+				System.out.print(nums[i][j]);
+				System.out.print(" ");
 			}
 			System.out.println();
+		}
+		
+		System.out.println("\nAccessing the elements\n");
+		//Accessing elements
+		int num[][] = new int [3][4];
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<4; j++) {
+				System.out.print(num[i][j] + "");
+				System.out.print(" ");
+			}
+			System.out.println(" ");
 		}
 		
 	}
