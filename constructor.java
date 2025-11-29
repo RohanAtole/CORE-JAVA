5298aff40b83e0b871d22c7bf560c2231a23c080
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-29 23:14:11 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+class Human1{
+	
+	public Human1() {
+		System.out.println("In constructor");
+	}
+}
+
+public class constructor{
+	public static void main(String args[]) {
+		Human1 h1 = new Human1();
+	}
+}
