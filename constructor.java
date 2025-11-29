09a3e78c41f08378d910a7f03c8397da2a1ffeb7
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-29 23:43:11 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -2,13 +2,28 @@ package OOPS;
 
 class Human1{
 	
+	private int age;
+	private String name;
+	
 	public Human1() {
+		
+		age = 12;
+		name = "John";
 		System.out.println("In constructor");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
 	}
 }
 
 public class constructor{
 	public static void main(String args[]) {
 		Human1 h1 = new Human1();
+		System.out.println(h1.getName() +":"+h1.getAge());
 	}
 }
