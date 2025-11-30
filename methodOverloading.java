8c5cf71b7d9075bc4edd0048e0a5406f07acc5c4
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-11-30 23:24:26 +0530

    Create methodOverloading.java

---
+++ methodOverloading.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+class A{
+	public A() {
+		super();
+		System.out.println("in A");
+	}
+	public A(int n) {
+		super();
+		System.out.println("in A int");
+	}
+}
+
+class B extends A{
+	
+	public B() {
+		super(5);
+		System.out.println("in B");
+	}
+	public B(int n) {
+		super();
+		System.out.println("in B int");
+	}
+}
+
+public class methodOverloading{
+	
+	public static void main(String args[]) {
+		
+	}
+}
