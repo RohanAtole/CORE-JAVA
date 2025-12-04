036da88903400f969c348fe222a7feacc7c7eba3
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-04 20:52:38 +0530

    Create RuntimePoly.java
    understand and implement

---
+++ polymorphism/RuntimePoly.java
@@ -0,0 +1,34 @@
+package oops.polymorphism;
+
+class A{
+	void show() {
+		System.out.println("In show method an class A");
+	}
+}
+class B extends A{
+	void show() {
+		System.out.println("In show method an class B");
+	}
+
+}
+class C extends B{
+	void show() {
+		System.out.println("In show method an class C");
+	}
+}
+
+public class RuntimePoly{
+	public static void main(String args[]) {
+		
+		//Dynamic method Dispatch
+		
+		A obj = new A(); //referring object as B but it is A.
+		obj.show();
+		
+		obj = new B(); //referring object as B.
+		obj.show();
+		
+		obj = new C();//referring object as C.
+		obj.show();
+	}
+}
