4414ab0f7dd1432e9245aa4b9daed27bdd1aade1
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-16 22:19:53 +0530

    Refactor Laptop object creation and comparison
    Created two Laptop objects and compared them for equality.

--- ObjectClass.java
+++ ObjectClass.java
@@ -11,10 +11,18 @@ class Laptop{
 
 public class ObjectClass{
 	public static void main(String args[]) {
-		Laptop obj = new Laptop();
-		obj.brand = "Lenona Yoga";
-		obj.price = 1000;
+		Laptop obj1 = new Laptop();
+		obj1.brand = "Lenona Yoga";
+		obj1.price = 1000;
 		
-		System.out.println(obj.toString());
+		Laptop obj2 = new Laptop();
+		obj2.brand = "Lenona Yoga";
+		obj2.price = 1000;
+		
+		boolean result = obj1.equals(obj2);
+		
+		System.out.println(result);
+		
+		System.out.println(obj1.toString());
 	}
 }
