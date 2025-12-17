7d5c2120101c74799bf58b80d5bb2a901e9419ee
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-18 00:39:00 +0530

    Add fly method to abstract class Car
    I've learn about more concept of abstract method, class, and how to defined and declared.

--- abstractKeyword.java
+++ abstractKeyword.java
@@ -4,6 +4,7 @@ abstract class Car{ //abstract Class
 	
 	//here, declared abstract method
 	public abstract void drive(); //understand abstract method should work in only abstract class
+	public abstract void fly();
 	
 	public void playMusic() {
 		System.out.println("Play music");
@@ -13,6 +14,10 @@ class WagonR extends Car{
 	public void drive() { //here, defined abstract method
 		System.out.println("drive wagonR Car");
 	}
+	
+	public void fly() {
+		System.out.println("car is flying");
+	}
 }
 
 public class abstractKeyword{
@@ -20,6 +25,7 @@ public class abstractKeyword{
 	public static void main(String args[]) {
 		Car obj = new WagonR();
 		obj.drive();
+		obj.fly();
 		obj.playMusic();
 	}
 }
