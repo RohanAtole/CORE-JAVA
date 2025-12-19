3ee927b5933fb384e534da5bf0b6aeaef269b08a
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-19 23:27:28 +0530

    Implement multiple interfaces in class F

--- interfaceImplement.java
+++ interfaceImplement.java
@@ -1,5 +1,9 @@
 package variablesdatatypes;
 
+//class - class -> extends
+//class - interface -> implements
+//interface - interface -> extends
+
 interface L{
 	
 	int age = 12;			//variables in interface is final and static.
@@ -9,20 +13,34 @@ interface L{
 	void config();
 }
 
-class F implements L{   //implements, if the interface is an
+interface C{
+	void run();
+}
+
+//In interface to interface inheritance is valid so use extends instead of implements.
+interface O extends C{
+	
+}
+
+//In interface have to implements multiple interfaces. 
+class F implements L,O{   //implements, if the interface is an
 	public void show() {
 		System.out.println("In show...");
 	}
 	public void config() {
 		System.out.println("In config.....");
 	}
+	public void run() {
+		System.out.println("In run......");
+	}
 }
 
 public class interfaceImplement{
 	public static void main(String args[]) {
-		L obj = new F();
+		F obj = new F();
 		obj.show();
 		obj.config();	
+		obj.run();
 		
 		//If the any variable is static them you can print directly using class name and interface name.
 		System.out.println(L.area);
