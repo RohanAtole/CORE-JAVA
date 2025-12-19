c2966d860ee2509bb5130703c1fb1e6d82eaa733
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-19 20:13:31 +0530

    Add 'config' method to abstract class 'J'
    Added a new abstract method 'config' to the abstract class 'J' and implemented it in the anonymous class.

--- abstractAndAnonymousClass.java
+++ abstractAndAnonymousClass.java
@@ -4,6 +4,7 @@ package variablesdatatypes;
 
 abstract class J{
 	public abstract void show();
+	public abstract void config(String msg); 
 }
 public class abstractAndAnonymousClass{
 	public static void main(String a[]) {
@@ -12,7 +13,12 @@ public class abstractAndAnonymousClass{
 				public void show() {
 					System.out.println("new show");
 				}
+				
+				public void config(String msg) {
+					System.out.print("config : "+msg);
+				}
 			};
 		obj.show();
+		obj.config("Successfully completed....");
 	}
 }
