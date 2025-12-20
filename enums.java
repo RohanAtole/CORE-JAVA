998631d23ad0f07b1df9c11420cc0b25b119e5e3
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-20 23:24:21 +0530

    Update enums.java

--- enums.java
+++ enums.java
@@ -6,9 +6,41 @@ enum Status{
 
 public class enums{
 	public static void main(String args[]) {
-		Status[] s = Status.values();
-		for(Status ss : s) {
-			System.out.println(ss +":"+ ss.ordinal());
+		
+		Status s = Status.success;
+		
+		//Using Switch Statement
+		
+		switch(s) {
+		case Running:
+			System.out.println("All Good");
+			break;
+			
+		case failed:
+			System.out.println("Try Again");
+			break;
+			
+		case pending:
+			System.out.println("Please Wait");
+			break;
+			
+		default : System.out.println("Done");
+			break;
+		}
+		
+		//Using the If-Else condition
+		
+		if(s == Status.Running) {
+			System.out.println("All Good");
+		}
+		else if(s == Status.failed) {
+			System.out.println("Try Again");
+		}
+		else if(s == Status.pending) {
+			System.out.println("Please Wait");
+		}
+		else {
+			System.out.println("Done");
 		}
 	}
 }
