49275c1c107232e2ac1f84d12366b70e2c0f0b2f
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-28 23:07:53 +0530

    Implement inheritance with classes Z and V

---
+++ annotationProgram.java
@@ -0,0 +1,19 @@
+package variablesdatatypes;
+
+class Z{
+	public void showTheDataWhichBelongsToThisClass() {
+		System.out.println("In Class Z method");
+	}
+}
+class V extends Z{
+	public void showTheDataWhichBelongToThisClass() {
+		System.out.println("In Class V method");
+	}
+}
+
+public class annotationProgram{
+	public static void main(String args[]) {
+		V obj = new V(); 
+		obj.showTheDataWhichBelongsToThisClass();
+	}
+}
