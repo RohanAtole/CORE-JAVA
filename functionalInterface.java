9691e51a8b92274affcc6f2a8ff04bbef9fb09c0
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-29 00:01:38 +0530

    Refactor functional interface and update lambda expression
    Removed the 'run' method from the functional interface and added a functional interface annotation. Updated the lambda expression syntax in the main method.

--- functionalInterface.java
+++ functionalInterface.java
@@ -1,12 +1,21 @@
 package variablesdatatypes;
 
+@FunctionalInterface
 interface G{
 	void show(); 
-	void run();
 }
 
+/*
+ * class M implements G{ public void show() {
+ * 
+ * } }
+ */
 public class functionalInterface{
 	public static void main(String args[]) {
-		
+		G obj = ()->    //Lambda Expression show using (->) that symbol.
+		{
+			System.out.println("In show");
+		};
+		obj.show();
 	}
 }
