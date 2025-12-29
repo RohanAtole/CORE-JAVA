0b5c4ed2c6311b1d31f4e2e3944347c84ae0ffbb
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-29 23:49:56 +0530

    Enhance exception handling and variable initialization
    Updated exception handling to include a catch for general exceptions and modified variable initialization.

--- Exception Handling/simpleExceptionHanding.java
+++ Exception Handling/simpleExceptionHanding.java
@@ -2,14 +2,16 @@ package variablesdatatypes;
 
 public class simpleExceptionHanding{
 	public static void main(String args[]) {
-		int i = 0;
+		int i = 2;
 		int j = 18;
 		int res;
 		int nums[] = new int[5];
+		String str = null;
 		
 		try
 		{
 			res = j/i;
+			System.out.println(str.length());
 			System.out.println(nums[1]);
 			System.out.println(nums[5]);
 		}
@@ -18,6 +20,9 @@ public class simpleExceptionHanding{
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
 		System.out.println("stay in your limit");
+		}
+		catch(Exception e) {
+			System.out.println("Something Went Wrong"+e);
 		}
 		System.out.println("Program Executed Successfully.....");
 	}
