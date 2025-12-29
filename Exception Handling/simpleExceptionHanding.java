a145f1c9cd35a6fcdfe3a63309281601f9229204
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-29 23:54:28 +0530

    Refactor exception handling in simpleExceptionHanding.java
    Improved exception handling with specified catch blocks and comments.

--- Exception Handling/simpleExceptionHanding.java
+++ Exception Handling/simpleExceptionHanding.java
@@ -8,21 +8,21 @@ public class simpleExceptionHanding{
 		int nums[] = new int[5];
 		String str = null;
 		
-		try
+		try   //this is try block to add suspicious block of code
 		{
 			res = j/i;
 			System.out.println(str.length());
 			System.out.println(nums[1]);
 			System.out.println(nums[5]);
 		}
-		catch(ArithmeticException e) {
+		catch(ArithmeticException e) {    //Specified Catch block
 			System.out.println("can not divide by zero");
 		}
-		catch(ArrayIndexOutOfBoundsException e) {
+		catch(ArrayIndexOutOfBoundsException e) { //This is also Specified Catch block
 		System.out.println("stay in your limit");
 		}
 		catch(Exception e) {
-			System.out.println("Something Went Wrong"+e);
+			System.out.println("Something Went Wrong"+e);//This is not Specified Catch block which is able catch any exception.
 		}
 		System.out.println("Program Executed Successfully.....");
 	}
