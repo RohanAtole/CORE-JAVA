455e8aa59087e720ab93bbbee07e767460bf3556
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2025-12-30 17:33:26 +0530

    Add throwExample class for arithmetic exception handling
    Implement exception handling with custom message.

---
+++ Exception Handling/throwExample.java
@@ -0,0 +1,22 @@
+package variablesdatatypes;
+
+public class throwExample{
+	public static void main(String args[]) {
+		int i = 20;
+		int j = 0;
+		
+		try {
+			j = 18/i;
+			if(j==0)
+				throw new ArithmeticException();
+		}
+		catch(ArithmeticException e){
+			j = 18/1;
+			System.out.println("Thats a default Output");
+		}
+		finally {
+			System.out.println(j);
+			System.out.println("Executed Successfully.....");
+		}
+	}
+}
