a5e516dfe4601b4b309906b43400e58a09ab77c1
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-01-02 19:07:45 +0530

    Create InputFromUser.java

---
+++ InputFromUser.java
@@ -0,0 +1,15 @@
+package variablesdatatypes;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputFromUser{
+	public static void main(String a[]){
+		System.out.println("Enter the number :");
+		
+		InputStreamReader in = new InputStreamReader(in);
+		BufferedReader bf = new BufferedReader(in);
+	}
+}
