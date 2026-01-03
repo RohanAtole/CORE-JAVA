f38a5349541577b99fadb402c37e95e554f36006
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-01-03 23:55:11 +0530

    Update and rename InputFromUser.java to InputFromUser1.java

--- InputFromUser1.java
+++ InputFromUser1.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
  
-public class InputFromUser{
+public class InputFromUser1{
 	public static void main(String a[]) throws IOException{
 		System.out.println("Enter the number :");
 		
