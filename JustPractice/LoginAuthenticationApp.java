467a734c4b53a80fe5b5d022ec5f81c8b2e3a545
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-01-06 23:20:23 +0530

    Create LoginAuthenticationApp.java

---
+++ JustPractice/LoginAuthenticationApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LoginAuthenticationApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+
+        if (username.equals("admin") && password.equals("admin123")) {
+            System.out.println("Login Successful. Welcome Admin.");
+        } else {
+            System.out.println("Login Failed. Invalid Credentials.");
+        }
+
+        scanner.close();
+    }
+}
