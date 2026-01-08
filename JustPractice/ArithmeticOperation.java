237067829dfb208e6e4556050bdedcb92cd31029
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-01-08 23:33:59 +0530

    Create ArithmeticOperation.java

---
+++ JustPractice/ArithmeticOperation.java
@@ -0,0 +1,13 @@
+class ArithmeticOperation
+{
+    public static void main(String args[])
+    {
+        int a=30;
+        int b=20;
+        System.out.println("addition is:"+(a+b));
+        System.out.println("substraction is:"+(a-b));
+        System.out.println("multiplication is:"+a*b);
+        System.out.println("division is:"+a/b);
+        System.out.println("module is:"+a%b);
+    }
+}
