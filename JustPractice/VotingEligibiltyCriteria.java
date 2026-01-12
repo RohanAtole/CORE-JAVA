1d9adc0df654cd0b5dd52f2b8896b2df3311c8c1
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-01-12 22:44:43 +0530

    Add VotingEligibilityCriteria class for age check

---
+++ JustPractice/VotingEligibiltyCriteria.java
@@ -0,0 +1,15 @@
+class VotingEligibiltyCriteria
+{
+public static void main(String args[])
+{
+int a=23;
+if(a>=18)
+{
+System.out.println("voting is eligible ");
+}
+else
+{
+System.out.println("voting is not eligible");
+}
+}
+}
