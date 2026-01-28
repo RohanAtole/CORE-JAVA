d79eda723d003564c35f34e72c5a303ccf35baba
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-01-28 21:44:25 +0530

    Refactor threading implementation to use Runnable

--- RunThreadProgram.java
+++ RunThreadProgram.java
@@ -1,23 +1,35 @@
 package variablesdatatypes;
 
-class H extends Thread{
+
+class H implements Runnable{
 	public void run() {
-		for(int i = 0; i <= 10; i++)
+		for(int i = 0; i <= 5; i++)
 		System.out.println("Hi");
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {e.printStackTrace();
+		}
 	}
 }
-class M extends Thread{
+class M implements Runnable{
 	public void run() {
-		for(int i = 0; i <= 10; i++)
+		for(int i = 0; i <= 5; i++)
 		System.out.println("Hello");
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {e.printStackTrace();
+		}
 	}
 }
 public class RunThreadProgram{
 	public static void main(String args[]) {
-		H obj1 = new H();
-		M obj2 = new M();
+		Runnable obj1 = new H();
+		Runnable obj2 = new M();
 		
-		obj1.start();
-		obj2.start(); 
+		Thread t1 = new Thread(obj1);
+		Thread t2 = new Thread(obj2);
+		
+		t1.start();
+		t2.start(); 
 	}
 }
