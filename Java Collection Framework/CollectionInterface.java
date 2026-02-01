d9bf47a0e16de5c65fa5959fde587c99c72475c1
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-01 19:09:45 +0530

    Implement main method in CollectionInterface class

--- Java Collection Framework/CollectionInterface.java
+++ Java Collection Framework/CollectionInterface.java
@@ -1,1 +1,22 @@
+package JCF;
 
+import java.util.ArrayList;
+
+import java.util.Collection;
+
+public class CollectionInterface {
+	public static void main(String args[]) {
+
+		Collection<Integer> nums = new ArrayList<Integer>();
+		
+		nums.add(3);
+		nums.add(5);
+		nums.add(8);
+		nums.add(2);
+		
+		for(int i : nums) {
+			int num = (Integer)i;
+			System.out.println(num);
+		}
+	}
+}
