487cf7c865766ed3112c2b8de86750a84df5b23c
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-01 20:45:33 +0530

    Modify CollectionInterface to enhance collection usage
    Updated the main method to print the entire collection instead of individual elements. Added List functionality to demonstrate indexOf and get methods.

--- Java Collection Framework/CollectionInterface.java
+++ Java Collection Framework/CollectionInterface.java
@@ -1,12 +1,14 @@
 package JCF;
 
 import java.util.ArrayList;
-
 import java.util.Collection;
+import java.util.List;
 
 public class CollectionInterface {
 	public static void main(String args[]) {
-
+		
+		//Collection
+		
 		Collection<Integer> nums = new ArrayList<Integer>();
 		
 		nums.add(3);
@@ -15,8 +17,20 @@ public class CollectionInterface {
 		nums.add(2);
 		
 		for(int i : nums) {
-			int num = (Integer)i;
-			System.out.println(num);
+			System.out.println(nums);
 		}
+		
+		//List
+		
+		List<Integer> num = new ArrayList<Integer>();
+		
+		num.add(6);
+		num.add(0);
+		num.add(1);
+		num.add(3);
+		
+		System.out.println(num.indexOf(0));
+		System.out.println(num.get(1));
+		
 	}
 }
