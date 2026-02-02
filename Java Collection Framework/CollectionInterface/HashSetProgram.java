dd6203b0ca06c71237e1d58d5bc0d9acbec1762f
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-02 20:55:39 +0530

    Rename HashSet class to HashSetProgram

--- Java Collection Framework/CollectionInterface/HashSetProgram.java
+++ Java Collection Framework/CollectionInterface/HashSetProgram.java
@@ -1,12 +1,8 @@
 package JCF;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-public class Set {
+public class HashSetProgram {
 	public static void main(String args[]) {
 	
 		//Set
