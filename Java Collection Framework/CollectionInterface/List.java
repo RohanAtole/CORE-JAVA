b78ad235c8ff026e5b7c69c7adfc3e067f53c807
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-02 18:10:40 +0530

    Rename CollectionInterface class to List

---
+++ Java Collection Framework/CollectionInterface/List.java
@@ -0,0 +1,31 @@
+package JCF;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class List {
+	public static void main(String args[]) {
+		
+		//List
+		
+		List<Integer> num = new ArrayList<Integer>();
+		
+		num.add(6);
+		num.add(0);
+		num.add(1);
+		num.add(3);
+		
+		System.out.println("List : ");
+		
+		System.out.println("Index Of : "+ num.indexOf(0));
+		System.out.println("Get Specific Number : "+ num.get(3));
+		
+		
+		System.out.println("In element in for loop : ");
+		
+		for (Object n : num) {
+			System.out.println(n);
+		}
+	}
+}
