2e0ba9d110120d84819e7ae2962f3e8de7b73ffd
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-02 20:52:46 +0530

    Add ArrayListProgram.java to CollectionInterface

--- Java Collection Framework/CollectionInterface/ArrayListProgram.java
+++ Java Collection Framework/CollectionInterface/ArrayListProgram.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class ArrayList {
+public class ArrayListProgram {
 	public static void main(String args[]) {
 		
 		//Collection
