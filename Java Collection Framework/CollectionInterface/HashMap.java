258a58521b6a5196d782468a4ae9ecd813f6d3c2
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-02 20:28:43 +0530

    Rename CollectionInterface class to HashMap

---
+++ Java Collection Framework/CollectionInterface/HashMap.java
@@ -0,0 +1,38 @@
+package JCF;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class HashMap {
+	public static void main(String args[]) {
+		
+		//Map
+		System.out.println("Map");
+		
+		Map<String, Integer> Students = new HashMap<>();
+		Students.put("Navin", 56);
+		Students.put("Harsh", 23);
+		Students.put("Sushil", 67);
+		Students.put("Kiran", 92);
+		Students.put("Harsh", 45);
+		
+		System.out.println(Students);
+		System.out.println(Students.get("Harsh"));
+		
+		//only print key's
+		System.out.println(Students.keySet());
+		
+		//Print key and Values
+		for (String name : Students.keySet())
+		{
+			System.out.println(name + " : " + Students.get(name));
+		}
+	}
+}
