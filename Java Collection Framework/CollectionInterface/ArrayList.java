9415edcbfb888e1363a148523206a10c82a9e132
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-02 18:09:33 +0530

    Refactor CollectionInterface to ArrayList class
    Removed List-related code and adjusted class structure.

--- Java Collection Framework/CollectionInterface/ArrayList.java
+++ Java Collection Framework/CollectionInterface/ArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class CollectionInterface {
+public class ArrayList {
 	public static void main(String args[]) {
 		
 		//Collection
@@ -19,18 +19,5 @@ public class CollectionInterface {
 		for(int i : nums) {
 			System.out.println(nums);
 		}
-		
-		//List
-		
-		List<Integer> num = new ArrayList<Integer>();
-		
-		num.add(6);
-		num.add(0);
-		num.add(1);
-		num.add(3);
-		
-		System.out.println(num.indexOf(0));
-		System.out.println(num.get(1));
-		
 	}
 }
