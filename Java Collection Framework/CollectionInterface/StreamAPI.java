5f493827e26857cfaecde28500df227a79c87d0c
Rohan Atole <158686367+RohanAtole@users.noreply.github.com>
2026-02-07 23:33:42 +0530

    Refactor sum calculation using Stream API

--- Java Collection Framework/CollectionInterface/StreamAPI.java
+++ Java Collection Framework/CollectionInterface/StreamAPI.java
@@ -3,22 +3,33 @@ package JCF;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import java.util.Collection;
 
 
 public class StreamAPI{
 	public static void main(String args[]) {
 		
-		List<Integer> num = Arrays.asList(4,2,3,6);
+		List<Integer> num = Arrays.asList(4,2,3,6,5,6);
 		
-		int sum = 0;
-		for (int n : num) {
-			if(n%2==0)
-			{
-				n = n + 2;
-				sum = sum + n;
-			}
-		}
-		System.out.println(sum);
+		//This multiple liner
+		Stream<Integer> s1 = num.stream();
+		
+		Stream<Integer> s2 = s1.filter(n -> n%2==0);
+		
+		Stream<Integer> s3 = s2.map(n -> n+2);
+		
+		int result = s3.reduce(0, (c,e) -> c+e);
+		
+		System.out.println(result);
+		
+		//One liner
+		int res = num.stream()
+			.filter(n -> n%2==0)
+			.map(n -> n+2)
+			.reduce(0, (c,e) -> c+e);
+		
+		System.out.println(res);
+		//s3.forEach(n -> System.out.println(n));
 	}
 }
